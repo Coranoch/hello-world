9cd26f82f03d2e04585ef3fe347fe9f965b6b6a0
Brett Martin <Brett.Martin3@PCC.EDU>
2019-04-07 17:14:26 -0700

    Create HelloWorld class

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+
+/**
+ * Say Hello
+ *
+ * @author Brett Martin
+ * @version 4/7/2019
+ */
+public class HelloWorld
+{
+    /**
+     * Say hello to the world
+     */
+    public void sayHello()
+    {
+        System.out.println("Hello, World!");
+    }
+}
